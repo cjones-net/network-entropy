8b011db59ffd373139aa42fa56d42e0457357859
cjones-net <chrispy.code@pm.me>
2025-11-06 11:16:24 +0100

    Format and rename vars for entropy plot script

--- entropies_crit_frac_compare.py
+++ entropies_crit_frac_compare.py
@@ -1,87 +1,166 @@
-import networkentropy as nent
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import entropy
 
-maxDegree = 1000
-expectedDegree = 10
+import networkentropy as nent
+
 FIG_DIR = Path("~/network-entropy/figures").expanduser()
 
-powerDegEnts = []
-powerRemEnts = []
-powerCrits = []
-minDegree = 1
-#iterates power law distributions over minimum degree values
-while minDegree < 10:
-    #finds value for alpha
-    alpha = nent.alpha_finder(minDegree,expectedDegree)
-    #generates power law degree distribution and measures degree distribution entropy
-    powerProbDist = nent.power_law_distribution(alpha,minDegree)
-    powerDegEnts.append(entropy(powerProbDist))
-    #finds first and second moments and calculates Molloy-Reed critical fraction
-    powerLawExpect = sum(k*p for k,p in zip(range(minDegree,maxDegree),powerProbDist))
-    powerLawExpectSquare = sum((k**2)*p for k,p in zip(range(minDegree,maxDegree),powerProbDist))
-    powerCrits.append(1-1/(powerLawExpectSquare/powerLawExpect-1))
-    #measures power law remaining degree entropy
-    powerRemEnts.append(entropy([k*p/powerLawExpect for k,p in zip(range(minDegree,maxDegree),powerProbDist)]))
-    #increases minimum degree value for next iteration
-    minDegree += 1
+max_degree = 1000
+expected_degree = 10
 
-logDegEnts = []
-logRemEnts = []
-logCrits = []
+power_law_degree_entropies = []
+power_law_remaining_entropies = []
+power_crit_fractions = []
+min_degree = 1
+# iterates power law distributions over minimum degree values
+while min_degree < 10:
+    # finds value for alpha
+    alpha = nent.alpha_finder(min_degree, expected_degree)
+    # generates power law degree distribution and measures degree distribution
+    # entropy
+    power_law_prob_dist = nent.power_law_distribution(alpha, min_degree)
+    power_law_degree_entropies.append(entropy(power_law_prob_dist))
+    # finds first and second moments and calculates Molloy-Reed critical
+    # fraction
+    power_law_expect_deg = sum(
+        k * p
+        for k, p in zip(range(min_degree, max_degree), power_law_prob_dist)
+    )
+    power_law_expect_deg_square = sum(
+        (k**2) * p
+        for k, p in zip(range(min_degree, max_degree), power_law_prob_dist)
+    )
+    power_crit_fractions.append(
+        1 - 1 / (power_law_expect_deg_square / power_law_expect_deg - 1)
+    )
+    # measures power law remaining degree entropy
+    power_law_remaining_entropies.append(
+        entropy(
+            [
+                k * p / power_law_expect_deg
+                for k, p in zip(
+                    range(min_degree, max_degree), power_law_prob_dist
+                )
+            ]
+        )
+    )
+    # increases minimum degree value for next iteration
+    min_degree += 1
+
+log_norm_degree_entropies = []
+log_norm_remaining_entropies = []
+log_norm_crit_fractions = []
 sigma = 0.2
-#iterates numerical log normal distributions over sigma values
+# iterates numerical log normal distributions over sigma values
 while sigma < 3:
-    #finds value for mu
-    mu = nent.mu_finder(sigma,expectedDegree)
-    #generates log normal degree distribution and measures degree distribution entropy
-    logProbDist = nent.log_normal_distribution(mu,sigma)
-    logDegEnts.append(entropy(logProbDist))
-    #finds first and second moments and calculates Molloy-Reed critical fraction
-    logNormExpect = sum(k*p for k,p in zip(range(maxDegree),logProbDist))
-    logNormExpectSquare = sum((k**2)*p for k,p in zip(range(maxDegree),logProbDist))
-    logCrits.append(1-1/(logNormExpectSquare/logNormExpect-1))
-    #measures log normal remaining degree entropy
-    logRemEnts.append(entropy([k*p/logNormExpect for k,p in zip(range(maxDegree),logProbDist)]))
-    #increases sigma value for next iteration
+    # finds value for mu
+    mu = nent.mu_finder(sigma, expected_degree)
+    # generates log normal degree distribution and measures degree distribution entropy
+    log_norm_prob_dist = nent.log_normal_distribution(mu, sigma)
+    log_norm_degree_entropies.append(entropy(log_norm_prob_dist))
+    # finds first and second moments and calculates Molloy-Reed critical fraction
+    log_norm_expect_deg = sum(
+        k * p for k, p in zip(range(max_degree), log_norm_prob_dist)
+    )
+    log_norm_expect_deg_square = sum(
+        (k**2) * p for k, p in zip(range(max_degree), log_norm_prob_dist)
+    )
+    log_norm_crit_fractions.append(
+        1 - 1 / (log_norm_expect_deg_square / log_norm_expect_deg - 1)
+    )
+    # measures log normal remaining degree entropy
+    log_norm_remaining_entropies.append(
+        entropy(
+            [
+                k * p / log_norm_expect_deg
+                for k, p in zip(range(max_degree), log_norm_prob_dist)
+            ]
+        )
+    )
+    # increases sigma value for next iteration
     sigma += 0.2
-    
-theoryDegEnts = []
-theoryRemEnts = []
-theoryCrits = []
-theorySigma = 0.01
-#iterates theoretical log normal distributions over sigma values
-while theorySigma < 3:
-    #calculates entropy values and Molloy Reed critical fraction
-    theoryDegEnts.append(0.5*(1-theorySigma**2) + np.log(expectedDegree*theorySigma*(2*np.pi)**0.5))
-    theoryRemEnts.append(0.5*(1+theorySigma**2) + np.log(expectedDegree*theorySigma*(2*np.pi)**0.5))
-    theoryCrits.append(1-1/(expectedDegree*np.exp(theorySigma**2)-1))
-    #increases sigma value for next iteration
-    theorySigma += 0.01
-    
-#plots degree distribution entropy against Molloy-Reed critical fraction
+
+theory_degree_entropies = []
+theory_remaining_entropies = []
+theory_crit_fractions = []
+theory_sigma = 0.01
+# iterates theoretical log normal distributions over sigma values
+while theory_sigma < 3:
+    # calculates entropy values and Molloy Reed critical fraction
+    theory_degree_entropies.append(
+        0.5 * (1 - theory_sigma**2)
+        + np.log(expected_degree * theory_sigma * (2 * np.pi) ** 0.5)
+    )
+    theory_remaining_entropies.append(
+        0.5 * (1 + theory_sigma**2)
+        + np.log(expected_degree * theory_sigma * (2 * np.pi) ** 0.5)
+    )
+    theory_crit_fractions.append(
+        1 - 1 / (expected_degree * np.exp(theory_sigma**2) - 1)
+    )
+    # increases sigma value for next iteration
+    theory_sigma += 0.01
+
+# plots degree distribution entropy against Molloy-Reed critical fraction
 plt.figure()
-plt.xlim(1,3.5)
-plt.ylim(0.88,1)
-plt.plot(powerDegEnts,powerCrits,'^',color = 'blue',label = 'Power-Law\n(numerical)')
-plt.plot(logDegEnts,logCrits,'s',color = 'red',label = 'Log-Normal\n(numerical)')
-plt.plot(theoryDegEnts,theoryCrits, linestyle = 'dashed', color = 'black',label = 'Log-Normal\n(theoretical)')
-plt.xlabel(r'$H(p)$',fontdict = {'fontsize':16})
-plt.ylabel(r'$f_c$',fontdict = {'fontsize':16},rotation=0)
-plt.legend(loc = 'upper left')
+plt.xlim(1, 3.5)
+plt.ylim(0.88, 1)
+plt.plot(
+    power_law_degree_entropies,
+    power_crit_fractions,
+    "^",
+    color="blue",
+    label="Power-Law\n(numerical)",
+)
+plt.plot(
+    log_norm_degree_entropies,
+    log_norm_crit_fractions,
+    "s",
+    color="red",
+    label="Log-Normal\n(numerical)",
+)
+plt.plot(
+    theory_degree_entropies,
+    theory_crit_fractions,
+    linestyle="dashed",
+    color="black",
+    label="Log-Normal\n(theoretical)",
+)
+plt.xlabel(r"$H(p)$", fontdict={"fontsize": 16})
+plt.ylabel(r"$f_c$", fontdict={"fontsize": 16}, rotation=0)
+plt.legend(loc="upper left")
 plt.savefig(FIG_DIR / "critical_frac_against_degree_dist_entropies.png")
 
-#plots remaining degree entropy against Molloy-Reed critical fraction
+# plots remaining degree entropy against Molloy-Reed critical fraction
 fig = plt.figure()
 ax = fig.add_subplot()
-plt.xlim(1,7)
-plt.ylim(0.88,1)
-plt.plot(powerRemEnts,powerCrits,'^',color = 'blue',label = 'Power-Law\n(numerical)')
-plt.plot(logRemEnts,logCrits,'s',color = 'red',label = 'Log-Normal\n(numerical)')
-plt.plot(theoryRemEnts,theoryCrits, linestyle = 'dashed', color = 'black',label = 'Log-Normal\n(theoretical)')
-ax.set_xlabel(r'$H(q)$',fontdict = {'fontsize':16})
-ax.set_ylabel(r'$f_c$',fontdict = {'fontsize':16},rotation=0)
-plt.legend(loc = 'upper left')plt.savefig(FIG_DIR / "critical_frac_against_remaining_dist_entropies.png")
+plt.xlim(1, 7)
+plt.ylim(0.88, 1)
+plt.plot(
+    power_law_remaining_entropies,
+    power_crit_fractions,
+    "^",
+    color="blue",
+    label="Power-Law\n(numerical)",
+)
+plt.plot(
+    log_norm_remaining_entropies,
+    log_norm_crit_fractions,
+    "s",
+    color="red",
+    label="Log-Normal\n(numerical)",
+)
+plt.plot(
+    theory_remaining_entropies,
+    theory_crit_fractions,
+    linestyle="dashed",
+    color="black",
+    label="Log-Normal\n(theoretical)",
+)
+ax.set_xlabel(r"$H(q)$", fontdict={"fontsize": 16})
+ax.set_ylabel(r"$f_c$", fontdict={"fontsize": 16}, rotation=0)
+plt.legend(loc="upper left")
+plt.savefig(FIG_DIR / "critical_frac_against_remaining_dist_entropies.png")
