36dac9ab0763a0c08c718f09eef4641869f45705
cjones-net <chrispy.code@pm.me>
2025-11-06 13:56:16 +0100

    Rename variables and functions

--- mutual_info_calcs.py
+++ mutual_info_calcs.py
@@ -4,81 +4,136 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 
-#generates the graph from an edge list file
-graph = nent.graph_from_file('network_data/fb-pages-tvshow.edges')
-nent.reduce_and_relabel(graph)
-#sets up the initial parameters and intervals for the swapping procedure
-swaps = [0,100,200,400,800,1600,3200]
 FIG_DIR = Path("~/network-entropy/figures").expanduser()
 
+# generates the graph from an edge list file
+graph = nent.entropyGraph()
+graph.graph_from_file(
+    Path("~/network-entropy/network_data/fb-pages-tvshow.edges").expanduser()
+)
+graph.reduce_and_relabel()
+# sets up the initial parameters and intervals for the swapping procedure
+swaps = [0, 100, 200, 400, 800, 1600, 3200]
 iterations = 100
-successCount = 0
-#initialises the lists for recording critical fraction and mutual information values
-randCritAve = []
-randError = []
-targCritAve = []
-targError = []
-standardMutuals = []
-clusterMutuals = []
+success_count = 0
+# initialises the lists for recording critical fraction and mutual information
+# values
+(
+    random_crit_frac_averages,
+    random_crit_frac_errors,
+    targeted_crit_frac_averages,
+    targeted_crit_frac_errors,
+    standard_mutual_information,
+    cluster_adjust_mutual_information,
+) = ([], [], [], [], [], [])
 
 #iterates over edge swaps until reaching a cutoff number of swaps
 while successCount <= max(swaps)+1:
     #at set intervals, records mutual information and critical fraction values
-    if successCount in swaps:
-        #informs the user when measurements are being taken
-        print('Successful swaps = ' + str(successCount))
-        standardMutuals.append(graph.mutual_info(clusterAdjust=False))
-        clusterMutuals.append(graph.mutual_info(clusterAdjust=True))
-        #simulates the critical fraction measurement multiple times in order to obtain an average
-        randCrits = [nent.sim_crit_frac(graph) for i in range(iterations)]
-        targCrits = [nent.sim_crit_frac(graph,targeting=True) for i in range(iterations)]
-        randCritAve.append(np.average(randCrits))
-        randError.append(np.std(randCrits))
-        targCritAve.append(np.average(targCrits))
-        targError.append(np.std(targCrits))
-    #performs a correlation preserving swap on the graph
-    nent.correlation_preserve_swap(graph,stayConnected=True)
-    successCount += 1
+    if success_count in swaps:
+        # informs the user when measurements are being taken
+        print("Successful swaps = " + str(success_count))
+        standard_mutual_information.append(
+            graph.mutual_information(cluster_adjust=False)
+        )
+        cluster_adjust_mutual_information.append(
+            graph.mutual_information(cluster_adjust=True)
+        )
+        # simulates the critical fraction measurement multiple times in order
+        # to obtain an average
+        random_crit_fracs = [
+            nent.critical_point_simulation(graph) for i in range(iterations)
+        ]
+        targeted_crit_fracs = [
+            nent.critical_point_simulation(graph, targeting=True)
+            for i in range(iterations)
+        ]
+        random_crit_frac_averages.append(np.average(random_crit_fracs))
+        random_crit_frac_errors.append(np.std(random_crit_fracs))
+        targeted_crit_frac_averages.append(np.average(targeted_crit_fracs))
+        targeted_crit_frac_errors.append(np.std(targeted_crit_fracs))
+    # performs a correlation preserving swap on the graph
+    nent.correlation_preserve_swap(graph, stay_connected=True)
 
 #plots random critical fraction against swaps
 plt.figure()
-plt.errorbar(y = randCritAve,x=swaps,yerr = randError,fmt ='s',capsize=2,color='green',label='Random Failure')
-plt.errorbar(y = randCritAve,x=swaps,color='green',linestyle='dashed')
-plt.xlim(-100,3500)
-plt.ylim(0.84,0.94)
+plt.errorbar(
+    y=random_crit_frac_averages,
+    x=swaps,
+    yerr=random_crit_frac_errors,
+    fmt="s",
+    capsize=2,
+    color="green",
+    label="Random Failure",
+)
+plt.errorbar(
+    y=random_crit_frac_averages, x=swaps, color="green", linestyle="dashed"
+)
+plt.xlim(-100, 3500)
 plt.legend()
 plt.ylabel(r"$f_c$", fontdict={"fontsize": 16}, rotation=0)
 plt.xlabel("Number of Edge Swaps", fontdict={"fontsize": 12})
 plt.savefig(FIG_DIR / "random_critical_frac_against_edge_swaps.png")
 
-#plots mutual information with clustering against random critical fraction
+# plots mutual information with clustering against random critical fraction
 plt.figure()
-plt.errorbar(x = randCritAve,y=clusterMutuals,xerr = randError,fmt ='s',capsize=2,color='green',label='Random Failure')
-plt.errorbar(x = randCritAve,y=clusterMutuals,color='green',linestyle='dashed')
-plt.ylim(0.3,1.2)
-plt.xlim(0.84,0.94)
+plt.errorbar(
+    x=random_crit_frac_averages,
+    y=cluster_adjust_mutual_information,
+    xerr=random_crit_frac_errors,
+    fmt="s",
+    capsize=2,
+    color="green",
+    label="Random Failure",
+)
+plt.errorbar(
+    x=random_crit_frac_averages,
+    y=cluster_adjust_mutual_information,
+    color="green",
+    linestyle="dashed",
+)
 plt.legend()
 plt.ylabel("Mutual Information with Clustering", fontdict={"fontsize": 12})
 plt.xlabel(r"$f_c$", fontdict={"fontsize": 16})
 plt.savefig(FIG_DIR / "mutual_information_against_random_critical_frac.png")
 
-#plots targeted critical fraction against swaps
+# plots targeted critical fraction against swaps
 plt.figure()
-plt.errorbar(y = targCritAve,x=swaps,yerr = targError,fmt ='o',capsize=2,color='purple',label='Targeted Attack')
-plt.errorbar(y = targCritAve,x=swaps,color='purple',linestyle='dashed')
-plt.xlim(-100,3500)
-plt.ylim(0.34,0.5)
+plt.errorbar(
+    y=targeted_crit_frac_averages,
+    x=swaps,
+    yerr=targeted_crit_frac_errors,
+    fmt="o",
+    capsize=2,
+    color="purple",
+    label="Targeted Attack",
+)
+plt.errorbar(
+    y=targeted_crit_frac_averages, x=swaps, color="purple", linestyle="dashed"
+)
+plt.xlim(-100, 3500)
 plt.legend()
 plt.ylabel(r"$f_c$", fontdict={"fontsize": 12}, rotation=0)
 plt.xlabel("Number of Edge Swaps", fontdict={"fontsize": 12})
 plt.savefig(FIG_DIR / "targeted_critical_frac_against_edge_swaps.png")
 
-#plots mutual information with clustering against targeted critical fraction
+# plots mutual information with clustering against targeted critical fraction
 plt.figure()
-plt.errorbar(x = targCritAve,y=clusterMutuals,xerr = targError,fmt ='o',capsize=2,color='purple',label='Targeted Attack')
-plt.errorbar(x = targCritAve,y=clusterMutuals,color='purple',linestyle='dashed')
-plt.ylim(0.3,1.2)
-plt.xlim(0.34,0.5)
+plt.errorbar(
+    x=targeted_crit_frac_averages,
+    y=cluster_adjust_mutual_information,
+    xerr=targeted_crit_frac_errors,
+    fmt="o",
+    capsize=2,
+    color="purple",
+    label="Targeted Attack",
+)
+plt.errorbar(
+    x=targeted_crit_frac_averages,
+    y=cluster_adjust_mutual_information,
+    color="purple",
+    linestyle="dashed",
+)
 plt.legend()
 plt.ylabel("Mutual Information with Clustering", fontdict={"fontsize": 12})
 plt.xlabel(r"$f_c$", fontdict={"fontsize": 16})
